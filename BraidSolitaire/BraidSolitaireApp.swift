aea163d1af069659b97f471115278a9eb92396b9
Joanna Kuehn <kuehnjoanna@gmail.com>
2024-11-08 17:02:38 +0100

    no message

---
+++ BraidSolitaire/BraidSolitaireApp.swift
@@ -0,0 +1,17 @@
+//
+//  BraidSolitaireApp.swift
+//  BraidSolitaire
+//
+//  Created by Joanna Kühn on 30.10.24.
+//
+
+import SwiftUI
+
+@main
+struct BraidSolitaireApp: App {
+    var body: some Scene {
+        WindowGroup {
+        //   GameView()
+        }
+    }
+}
